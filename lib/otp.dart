6076a75923d9b46a7d40e65e1b2cf64062fcfbd1
AnshGit-hash <73894697+AnshGit-hash@users.noreply.github.com>
2024-11-30 17:44:06 +0530

    first

--- lib/otp.dart
+++ lib/otp.dart
@@ -1,133 +1,127 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class OTPLogin extends StatelessWidget {
+  const OTPLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 106,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        offset: const Offset(0, 5),
-                        blurRadius: 0.5,
-                        spreadRadius: 2,
-                      ),
-                    ],
-                    color: Colors.grey,
-                    image: const DecorationImage(
-                      image: AssetImage('assets/bg1.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30.0),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: InkWell(
-                            onTap: () {
-                              Navigator.of(context).pop();
-                            },
-                            child: Image.asset(
-                              'assets/back.png',
-                              width: 30,
-                              height: 30,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          'Wishlist',
-                          style: TextStyle(
-                            color: Colors.pink.shade600,
-                            fontFamily: 'DMSerifDisplay',
-                            fontSize: 25,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-
-                Center(
-        child: Container(
-          width: 300, 
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                blurRadius: 10,
-                offset: const Offset(0, 5),
-              ),
-            ],
-          ),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
           child: Column(
-            mainAxisSize: MainAxisSize.min,
             children: [
               Container(
-                height: 300, 
+                height: 106,
                 decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      offset: const Offset(0, 5),
+                      blurRadius: 0.5,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                  color: Colors.grey,
                   image: const DecorationImage(
-                    image: AssetImage('assets/your_image.png'), 
+                    image: AssetImage('assets/bg1.jpg'),
                     fit: BoxFit.cover,
                   ),
-                  borderRadius: BorderRadius.circular(8),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Your wishlist is empty',
-                style: TextStyle(
-                  color: Colors.pink,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20), 
-              Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(2)),
-                child: ElevatedButton(
-                  onPressed: () {
-                  
-                  },
-                   style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(194, 199, 0, 113),
-                    foregroundColor: Colors.white, 
-                     shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(7),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Image.asset(
+                            'assets/back.png',
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Wishlist',
+                        style: TextStyle(
+                          color: Colors.pink.shade600,
+                          fontFamily: 'DMSerifDisplay',
+                          fontSize: 25,
+                        ),
+                      ),
+                    ],
                   ),
-                
                 ),
-                  child: const Text('Go to Shop'),
               ),
-          )],
-          ),
-             )) ],
-            ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                  child: Container(
+                width: 300,
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 10,
+                      offset: const Offset(0, 5),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: 300,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 2),
+                        image: const DecorationImage(
+                          image: AssetImage('assets/your_image.png'),
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Your wishlist is empty',
+                      style: TextStyle(
+                        color: Colors.pink,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration:
+                          BoxDecoration(borderRadius: BorderRadius.circular(2)),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(194, 199, 0, 113),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                        ),
+                        child: const Text('Go to Shop'),
+                      ),
+                    )
+                  ],
+                ),
+              ))
+            ],
           ),
         ),
       ),
